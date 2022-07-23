1ae2f8251fd1b9554e1539e8adee0efe1aba11f2
Himanshulohar <slohar717@gmail.com>
2022-07-23 15:46:31 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header';
+import Tasks from './components/Tasks';
+import { useState } from 'react';
+import AddTask from './components/AddTask';
+const App = () => {
+  const [showAddTask, setshowAddTask] = useState(true);
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Doctor's Appointment",
+      day: 'Feb 5th at 2:30pm',
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: 'Football',
+      day: 'Feb 6th at 2:30pm',
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: 'Dentist',
+      day: 'Feb 7th at 2:30pm',
+      reminder: true,
+    },
+  ]);
 
-function App() {
+  const addTask = task => {
+    const id = Math.floor(Math.random() * 100) + 1;
+    console.log(id);
+    const newTask = { id, ...task };
+    setTasks([...tasks, newTask]);
+  };
+  const deletetask = id => {
+    setTasks(tasks.filter(task => task.id !== id));
+  };
+  const toggleReminder = id => {
+    setTasks(
+      tasks.map(task =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <Header
+        onAdd={() => {
+          setshowAddTask(!showAddTask);
+        }}
+        showAdd={showAddTask}
+      />
+      {showAddTask && <AddTask onAdd={addTask} />}
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deletetask} onToggle={toggleReminder} />
+      ) : (
+        'Nothing to show MF'
+      )}
     </div>
   );
-}
+};
 
 export default App;
